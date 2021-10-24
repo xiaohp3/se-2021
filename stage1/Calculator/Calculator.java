ac7930f27f5c002e5691c401d105cfdc73d2d3d3
xiaohp3 <2585300399@qq.com>
2021-10-24 11:05:42 +0800

    1

--- stage1/Calculator/Calculator.java
+++ stage1/Calculator/Calculator.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
+
 public class Calculator extends JFrame implements ActionListener {
     // data variable
     private double num1;     // the first number to do compute
